5038fd611c066dd583a998468bd52d3193771873
SAHB <s75ahmad@uwaterloo.ca>
2024-01-26 14:56:22 -0500

    use angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule, BrowserAnimationsModule ],
+  imports: [ BrowserModule, FormsModule, BrowserAnimationsModule, MatInputModule, MatButtonModule ],
   declarations: [AppComponent, PostCreateComponent],
   bootstrap: [AppComponent]
 })
