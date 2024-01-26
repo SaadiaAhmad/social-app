f2a6b55b87c1616f5b6489be5ec5c90af73ded2f
SAHB <s75ahmad@uwaterloo.ca>
2024-01-26 14:55:19 -0500

    add angular material lib dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule ],
+  imports: [ BrowserModule, FormsModule, BrowserAnimationsModule ],
   declarations: [AppComponent, PostCreateComponent],
   bootstrap: [AppComponent]
 })
