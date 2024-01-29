1117bff975c9d02233bdd919051d8cf10d4bdbc2
SAHB <s75ahmad@uwaterloo.ca>
2024-01-29 10:48:46 -0500

    update and use header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { HeaderComponent } from './header/header.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule, BrowserAnimationsModule, MatInputModule, MatButtonModule, MatCardModule ],
-  declarations: [AppComponent, PostCreateComponent],
+  imports: [ 
+    BrowserModule, 
+    FormsModule, 
+    BrowserAnimationsModule, 
+    MatInputModule, 
+    MatButtonModule, 
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule
+  ],
+  declarations: [
+    AppComponent, 
+    PostCreateComponent,
+    HeaderComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
