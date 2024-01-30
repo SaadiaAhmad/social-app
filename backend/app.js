14ce520b36c790b554f13a47490566236388fc60
SAHB <s75ahmad@uwaterloo.ca>
2024-01-30 13:24:30 -0500

    add GET posts endpoint

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('First middleware...');
-    next();
-});
-
-app.use((req, res, next) => {
-    res.send('Hello from express!');
+app.use('/api/posts', (req, res, next) => {
+    const posts = [
+        {
+            id: '123123',
+            title: 'First post from backend',
+            content: 'This is coming from the server app as I work on building a MEAN app!'
+        },
+        {
+            id: '456456',
+            title: 'Second post from backend',
+            content: 'This is also coming from the server app as I work on building a MEAN app!'
+        },
+    ]
+    res.status(200).json({
+        message: 'Get Posts Successful',
+        posts: posts
+    })
 });
 
 module.exports = app;
