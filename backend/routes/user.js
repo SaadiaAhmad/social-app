391c9088f298c3729e5d11bd870025f456a60b13
SAHB <s75ahmad@uwaterloo.ca>
2024-02-07 14:37:49 -0500

    add signup POST endpoint to the BE

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/user');
 
 const router = express.Router();
 
 router.post("/signup", (req, res, next) => {
-
+    bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            const user = new User({
+                email: req.body.email,
+                password: hash
+            });
+            user.save()
+                .then(result => {
+                    res.status(201).json({
+                        message: 'User creadted!',
+                    })
+                })
+                .catch(err => {
+                    res.status(400).json({
+                        message: 'Error when trying to create user!',
+                    })
+                })
+        });
 });
 
 module.exports = router;
