ecb1fde89d0f397c2e4e80f530b3c4c647496064
SAHB <s75ahmad@uwaterloo.ca>
2024-02-08 13:37:08 -0500

    use auth status in header

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
 import { User } from './auth-data.model';
 import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private token: string;
+  private authStatus$ = new Subject<boolean>();
 
   constructor(private httpClient: HttpClient) { }
 
   getToken(): string {
     return this.token;
+  }
+
+  getAuthStatusListener() {
+    return this.authStatus$.asObservable();
   }
 
   createUser(user: User) {
@@ -23,6 +29,7 @@ export class AuthService {
     this.httpClient.post<{ message: string, token: string }>('http://localhost:3000/api/user/login', user)
     .subscribe(data => {
       this.token = data.token;
+      this.authStatus$.next(true);
     });
   }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  private subscription: Subscription;
+  isUserAuthenticated = false;
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.subscription = this.authService.getAuthStatusListener().subscribe(isAuthenicated => {
+      this.isUserAuthenticated = isAuthenicated;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
