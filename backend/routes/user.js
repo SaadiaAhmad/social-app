85b39a331ae6c781f0a8cb1a8d2c47d5d2580a69
SAHB <s75ahmad@uwaterloo.ca>
2024-02-08 10:10:38 -0500

    add login logic in the BE

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -15,15 +16,48 @@ router.post("/signup", (req, res, next) => {
             user.save()
                 .then(result => {
                     res.status(201).json({
-                        message: 'User creadted!',
+                        message: 'User creadted!'
                     })
                 })
                 .catch(err => {
                     res.status(400).json({
                         message: 'Error when trying to create user!',
+                        error: err
                     })
                 })
         });
+});
+
+//Refactor this if possible. Maybe use nested promise handling
+router.post("/login", (req, res, next) => {
+    let userMatch;
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if(!user) {
+                return res.status(401).json({
+                    message: 'Invalid email for user'
+                })
+            }
+            userMatch = user;
+            return bcrypt.compare(req.body.password, user.password)
+        })
+        .then(result => {
+            if(!result) {
+                return res.status(401).json({
+                    message: 'Invalid password for user'
+                })
+            }
+            const token = jwt.sign(
+                { email: req.body.email, userId: userMatch._id }, 
+                'secret_should_be_longer_than_this',
+                { expiresIn: '1h' });
+        })
+        .catch(err => {
+            res.status(401).json({
+                message: 'Authorization failed!',
+                error: err
+            })
+        })
 });
 
 module.exports = router;
