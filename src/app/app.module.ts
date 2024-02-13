c87c6ad1aadafebbff68733ed54e513405bb74d0
SAHB <s75ahmad@uwaterloo.ca>
2024-02-13 11:55:10 -0500

    add angular material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,44 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { HeaderComponent } from './header/header.component';
-import { MatIconModule } from '@angular/material/icon';
 import { PostListComponent } from './posts/post-list/post-list.component';
-import { MatExpansionModule } from '@angular/material/expansion';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatPaginatorModule } from '@angular/material/paginator';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { ErrorInterceptor } from './error-interceptor';
-import { MatDialogModule } from '@angular/material/dialog';
 import { ErrorComponent } from './error/error.component';
+import { AngularMaterialModule } from './angular-material.module';
 
 @NgModule({
   imports: [ 
     AppRoutingModule,
     BrowserModule, 
     BrowserAnimationsModule, 
-    MatInputModule, 
-    MatButtonModule, 
-    MatCardModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatExpansionModule,
     HttpClientModule,
     RouterModule,
-    MatProgressSpinnerModule,
     ReactiveFormsModule,
-    MatPaginatorModule,
     FormsModule,
-    MatDialogModule
+    AngularMaterialModule
   ],
   declarations: [
     AppComponent, 
